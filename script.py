3f30c85270ba0f77b7ec40123e176667f92f96ed
BorjaRuizReverter <borjaruizreverter@gmail.com>
2023-08-28 14:33:57 -0300

    feat: recalibration and moving config to file

--- script.py
+++ script.py
@@ -1,11 +1,12 @@
 '''
-Firt, let's import the required packages
+Firt, import the required packages
 '''
 import os
 import emoji
 import keras_nlp
+import config
 
-os.environ["KERAS_BACKEND"] = "tensorflow"  # or "tensorflow" or "torch"
+os.environ["KERAS_BACKEND"] = config.keras_backend
 
 '''
 This Basic Sentiment Analysis is based on a pretrained backbone taken from keras_npl models
@@ -13,23 +14,26 @@ This Basic Sentiment Analysis is based on a pretrained backbone taken from keras
 classifier = keras_nlp.models.BertClassifier.from_preset("bert_tiny_en_uncased_sst2")
 
 '''
-Now type in the sentence that it is going to be analysed
+Now the user should type in the sentence that it will be analysed
 '''
 sentence = input("Type a sentence: ")
 sentiment_matrix = classifier.predict([sentence])
 
 '''
-This is to print the result in terms of a 1x2 matrix
+This is to print the result in terms of a 1x2 polarization matrix
 '''
 print("The result of this binary classification is {} negative and {} positive".format(sentiment_matrix[0,0], sentiment_matrix[0,1]))
 
-if (sentiment_matrix[0,0] >= 1.5):
+'''
+The following lines are just to transform numbers into emojis. The limits are only based on experience.
+'''
+if (sentiment_matrix[0,0] >= 1.8):
     print(emoji.emojize('Therefore, this sentence is classified as very negative :thumbs_down: :thumbs_down:'))
 elif (sentiment_matrix[0,0] >= 0.5):
     print(emoji.emojize('Therefore, this sentence is classified as negative :thumbs_down:'))
 elif (sentiment_matrix[0,0] >= -0.5):
     print(emoji.emojize('Therefore, this sentence is classified as neutral :man_shrugging:'))
-elif (sentiment_matrix[0,0] >= -1.5):
+elif (sentiment_matrix[0,0] >= -1.8):
     print(emoji.emojize('Therefore, this sentence is classified as positive :thumbs_up:'))
-else: #sentiment_matrix[0,0] < -1.5
+else: #sentiment_matrix[0,0] < -1.8
     print(emoji.emojize('Therefore, this sentence is classified as very positive :thumbs_up: :thumbs_up:'))
